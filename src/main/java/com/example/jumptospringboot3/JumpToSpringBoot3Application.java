f6830aaf10b5559674aa1b0817cdcdd46e062d24
SeoPBi <aktms252@naver.com>
2024-01-09 22:57:41 +0900

    240109

---
+++ src/main/java/com/example/jumptospringboot3/JumpToSpringBoot3Application.java
@@ -0,0 +1,13 @@
+package com.example.jumptospringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumpToSpringBoot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JumpToSpringBoot3Application.class, args);
+    }
+
+}
